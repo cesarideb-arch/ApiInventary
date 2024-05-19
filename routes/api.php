c221b9bc400a825285448997b32a14bfb8633fb0
Josue Montellano <2122100647@soy.utj.edu.mx>
2024-05-19 12:36:51 -0600

    Actualizar controladores de categoría, proyecto y proveedor para incluir verificación de relaciones antes de eliminar registros

--- routes/api.php
+++ routes/api.php
@@ -7,10 +7,10 @@ use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\SupplierController;
 use App\Http\Controllers\ProjectController;
-use App\Http\Controllers\EntranceController; // Add this line
-use App\Http\Controllers\OutputController; // Add this line
-use App\Http\Controllers\LoanController; // Add this line
-use App\Http\Controllers\CategoryController; // Add this line
+use App\Http\Controllers\EntranceController;
+use App\Http\Controllers\OutputController;
+use App\Http\Controllers\LoanController;
+use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,81 +25,69 @@ use App\Http\Controllers\CategoryController; // Add this line
 
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
-    
-//Rutas de productos
-Route::get('/products', [ProductController::class, 'index']);
-Route::post('/products', [ProductController::class, 'store']);
-Route::get('/products/{id}', [ProductController::class, 'show']);
-Route::put('/products/{id}', [ProductController::class, 'update']);
-Route::delete('/products/{id}', [ProductController::class, 'destroy']);
-Route::get('/getCountProducts', [ProductController::class, 'getCountProducts']);
-Route::get('/getprojects', [ProductController::class, 'getprojects']);
-//Rutas de perzolizacion    
-Route::get('/getCategoryProducts', [ProductController::class, 'getCategoryProducts']);
-Route::get('/search', [ProductController::class, 'SearchGet']);
-
-
-
-//Rutas de productos
-Route::get('/suppliers', [SupplierController::class, 'index']);
-Route::post('/suppliers', [SupplierController::class, 'store']);
-Route::get('/suppliers/{id}', [SupplierController::class, 'show']);
-Route::put('/suppliers/{id}', [SupplierController::class, 'update']);
-Route::delete('/suppliers/{id}', [SupplierController::class, 'destroy']);
-Route::get('/searchSupplier', [SupplierController::class, 'SearchSupplier']);
-
-//Rutas de projects
-Route::get('/projects', [ProjectController::class, 'index']);
-Route::post('/projects', [ProjectController::class, 'store']);
-Route::get('/projects/{id}', [ProjectController::class, 'show']);
-Route::put('/projects/{id}', [ProjectController::class, 'update']);
-Route::delete('/projects/{id}', [ProjectController::class, 'destroy']);
-Route::get('/searchProject', [ProjectController::class, 'SearchProject']);
-
-
-
-// Rutas de categories
-Route::get('/categories', [CategoryController::class, 'index']);
-Route::post('/categories', [CategoryController::class, 'store']);
-Route::get('/categories/{id}', [CategoryController::class, 'show']);
-Route::put('/categories/{id}', [CategoryController::class, 'update']);
-Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
-Route::get('/searchCategory', [CategoryController::class, 'SearchCategory']);
-
-
-
-
-// Add entrace
-Route::post('/entrances', [EntranceController::class, 'store']);
-Route::get('/entrances', [EntranceController::class, 'index']);
-Route::get('/entrances/{id}', [EntranceController::class, 'show']);
-Route::put('/entrances/{id}', [EntranceController::class, 'update']);
-Route::delete('/entrances/{id}', [EntranceController::class, 'destroy']);
-Route::get('/searchEntrance', [EntranceController::class, 'SearchEntrance']);
-
-// Add Output
-Route::post('/outputs', [OutputController::class, 'store']);
-Route::get('/outputs', [OutputController::class, 'index']);
-Route::get('/outputs/{id}', [OutputController::class, 'show']);
-Route::put('/outputs/{id}', [OutputController::class, 'update']);
-Route::delete('/outputs/{id}', [OutputController::class, 'destroy']);
-Route::get('/searchOutput', [OutputController::class, 'SearchOutput']);
-
-
-
-// Add Loans
-Route::post('/loans', [LoanController::class, 'store']);
-Route::get('/loans', [LoanController::class, 'index']);
-Route::get('/loans/{id}', [LoanController::class, 'show']);
-Route::put('/loans/{id}', [LoanController::class, 'update']);
-Route::delete('/loans/{id}', [LoanController::class, 'destroy']);
-Route::put('/comeBackLoan/{id}', [LoanController::class, 'comeBackLoan']);
-Route::get('/getCount', [LoanController::class, 'getCount']);
-Route::get('/searchLoan', [LoanController::class, 'SearchLoan']);
-
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('logout', [AuthController::class, 'logout']);
 
- 
-}); // Add this closing bracket
+    // Rutas de productos
+    Route::get('/products', [ProductController::class, 'index']);
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::get('/products/{id}', [ProductController::class, 'show']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+    Route::get('/getCountProducts', [ProductController::class, 'getCountProducts']);
+    Route::get('/getprojects', [ProductController::class, 'getprojects']);
+    // Rutas de personalización    
+    Route::get('/getCategoryProducts', [ProductController::class, 'getCategoryProducts']);
+    Route::get('/search', [ProductController::class, 'SearchGet']);
+
+    // Rutas de proveedores
+    Route::get('/suppliers', [SupplierController::class, 'index']);
+    Route::post('/suppliers', [SupplierController::class, 'store']);
+    Route::get('/suppliers/{id}', [SupplierController::class, 'show']);
+    Route::put('/suppliers/{id}', [SupplierController::class, 'update']);
+    Route::delete('/suppliers/{id}', [SupplierController::class, 'destroy']);
+    Route::get('/searchSupplier', [SupplierController::class, 'SearchSupplier']);
+
+    // Rutas de proyectos
+    Route::get('/projects', [ProjectController::class, 'index']);
+    Route::post('/projects', [ProjectController::class, 'store']);
+    Route::get('/projects/{id}', [ProjectController::class, 'show']);
+    Route::put('/projects/{id}', [ProjectController::class, 'update']);
+    Route::delete('/projects/{id}', [ProjectController::class, 'destroy']);
+    Route::get('/searchProject', [ProjectController::class, 'SearchProject']);
+
+    // Rutas de categorías
+    Route::get('/categories', [CategoryController::class, 'index']);
+    Route::post('/categories', [CategoryController::class, 'store']);
+    Route::get('/categories/{id}', [CategoryController::class, 'show']);
+    Route::put('/categories/{id}', [CategoryController::class, 'update']);
+    Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
+    Route::get('/searchCategory', [CategoryController::class, 'SearchCategory']);
+
+    // Rutas de entradas
+    Route::post('/entrances', [EntranceController::class, 'store']);
+    Route::get('/entrances', [EntranceController::class, 'index']);
+    Route::get('/entrances/{id}', [EntranceController::class, 'show']);
+    Route::put('/entrances/{id}', [EntranceController::class, 'update']);
+    Route::delete('/entrances/{id}', [EntranceController::class, 'destroy']);
+    Route::get('/searchEntrance', [EntranceController::class, 'SearchEntrance']);
+
+    // Rutas de salidas
+    Route::post('/outputs', [OutputController::class, 'store']);
+    Route::get('/outputs', [OutputController::class, 'index']);
+    Route::get('/outputs/{id}', [OutputController::class, 'show']);
+    Route::put('/outputs/{id}', [OutputController::class, 'update']);
+    Route::delete('/outputs/{id}', [OutputController::class, 'destroy']);
+    Route::get('/searchOutput', [OutputController::class, 'SearchOutput']);
+
+    // Rutas de préstamos
+    Route::post('/loans', [LoanController::class, 'store']);
+    Route::get('/loans', [LoanController::class, 'index']);
+    Route::get('/loans/{id}', [LoanController::class, 'show']);
+    Route::put('/loans/{id}', [LoanController::class, 'update']);
+    Route::delete('/loans/{id}', [LoanController::class, 'destroy']);
+    Route::put('/comeBackLoan/{id}', [LoanController::class, 'comeBackLoan']);
+    Route::get('/getCount', [LoanController::class, 'getCount']);
+    Route::get('/searchLoan', [LoanController::class, 'SearchLoan']);
+});
