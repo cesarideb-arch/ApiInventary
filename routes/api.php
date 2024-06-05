a4dffe3a506232359ebfc244ec6f339057852161
Josue Montellano <2122100647@soy.utj.edu.mx>
2024-06-04 21:21:27 -0600

    Actualizar controlador de registro para habilitar el registro de usuarios

--- routes/api.php
+++ routes/api.php
@@ -23,11 +23,12 @@ use App\Http\Controllers\CategoryController;
 |
 */
 
-Route::post('register', [AuthController::class, 'register']);
+// Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('register', [AuthController::class, 'register']);
 
     // Rutas de usuarios
     Route::get('/users', [AuthController::class, 'index']);
