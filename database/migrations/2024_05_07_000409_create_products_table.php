9799943369a927b6bc8cb5e2f18af7f7beecc12a
Josue Montellano <2122100647@soy.utj.edu.mx>
2024-06-11 12:18:46 -0600

    chore: Make price field nullable in products table migration

--- database/migrations/2024_05_07_000409_create_products_table.php
+++ database/migrations/2024_05_07_000409_create_products_table.php
@@ -19,7 +19,7 @@ return new class extends Migration {
             $table->string('brand')->nullable();
             $table->integer('quantity');
             $table->text('description')->nullable();
-            $table->decimal('price', 8, 2);
+            $table->decimal('price', 8, 2)->nullable();
             $table->string('profile_image')->nullable();
             $table->string('serie')->nullable();
             $table->string('observations', 50)->nullable();
