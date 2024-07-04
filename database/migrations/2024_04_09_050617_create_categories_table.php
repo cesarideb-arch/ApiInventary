494083fd009324c047de5d06c2ccc6b13c2b0759
Josue Montellano <2122100647@soy.utj.edu.mx>
2024-07-04 11:03:10 -0600

    cambio de migration

--- database/migrations/2024_04_09_050617_create_categories_table.php
+++ database/migrations/2024_04_09_050617_create_categories_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
          Schema::create('categories', function (Blueprint $table) {
              $table->id();
              $table->string('name', 100);
-             $table->string('description', 100)->nullable();
+             $table->string('description', 500)->nullable();
              $table->timestamps();
          });
      }
